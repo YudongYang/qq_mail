ce224ed185608bfbc3b1d38d71f473c7b128879c
YuDongYang <729061286@qq.com>
2017-09-13 00:37:37 +0800

    JS init

--- html/js/xlogin.js
+++ html/js/xlogin.js
@@ -1,16 +1,26 @@
 /* xlogin js */
-var tabSelect = 1
+var selectedTab = 1
 
-console.log($)
+$('#switcher_qlogin').click(function () {
+    selectedTab = 1
+    selectTab(selectedTab)
+})
 
-$('#switcher_qlogin').click(function() {
-    console.log(1)
-});
+$('#switcher_plogin').click(function () {
+    selectedTab = 2
+    selectTab(selectedTab)
+})
 
-console.log($('#switcher_plogin').click())
-
-console.log('label')
-
-$('#web_login_panel').hide()
-
-var selectTab = function() {}
+var selectTab = function (tabIndex) {
+    if (tabIndex === 1) {
+        $('#qr_login_panel').show()
+        $('#switcher_qlogin').attr('class', 'switch_btn_focus')
+        $('#web_login_panel').hide()
+        $('#switcher_plogin').attr('class', 'switch_btn')
+    } else {
+        $('#web_login_panel').show()
+        $('#switcher_plogin').attr('class', 'switch_btn_focus')
+        $('#qr_login_panel').hide()
+        $('#switcher_qlogin').attr('class', 'switch_btn')
+    }
+}
